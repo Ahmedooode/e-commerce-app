c2b244090b3045de583ef58cd8ae9dc937e5d99b
ahmedooode <ahmednasrehab2019@gmail.com>
2025-11-10 15:34:30 +0200

    fix the generate of payload types , e.g.in this commit categories was generated successfully

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -2,6 +2,11 @@ import type { CollectionConfig } from "payload";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
+  // this commented-out access control is just an example of how to restrict creation and deletion of categories
+  // access: {
+  //   create: () => false,
+  //   delete: () => false,
+  // },
   fields: [
     {
       name: "name",
